0a93f2d799f44337154f4f5293a4c7defd91c442
MikhailZhi <zhirovmal@gmail.com>
2023-04-19 17:55:27 +0500

    Project about math module

---
+++ Using_math_module.py
@@ -0,0 +1,27 @@
+#just stretch my fingers
+name='Mike'
+print(f'\nHello, {name}. Glad to see you again!\n')
+print('He isn\'t here\n') #Trying to see a special character(') in a printied string
+
+
+import math
+
+
+a = 7
+b = 4
+print('a =', a, ' b =', b)
+print('a + b = ', a + b)
+print('a * b = ', a * b)
+print('a - b = ', a - b)
+print('a / b = ', a / b, '\n')
+print('a % b = ', a % b)
+print('a // b = ', a // b)
+print('-a  = ', -a)
+print('a + b = ', a + b, '\n')
+
+
+# examples for "math" module
+print('Round: a / b = ', round(a / b))
+print('math.floor: a / b = ', math.floor(a / b))
+print('math.ceil: a / b = ', math.ceil(a / b), '\n')
+print('math.pi = ', math.pi)
