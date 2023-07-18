18a05c9fc5416c8251ea6285242a94f873c4acd2
MikhailZhi <zhirovmal@gmail.com>
2023-07-18 14:54:34 +0500

    Updated the code with pep8

--- Using_math_module.py
+++ Using_math_module.py
@@ -1,11 +1,9 @@
-#just stretch my fingers
-name='Mike'
-print(f'\nHello, {name}. Glad to see you again!\n')
-print('He isn\'t here\n') #Trying to see a special character(') in a printied string
-
-
 import math
 
+# just stretch my fingers
+name = 'Mike'
+print(f'\nHello, {name}. Glad to see you again!\n')
+print('He isn\'t here\n')  # Trying to see a special character(') in a printed string
 
 a = 7
 b = 4
@@ -18,7 +16,6 @@ print('a % b = ', a % b)
 print('a // b = ', a // b)
 print('-a  = ', -a)
 print('a + b = ', a + b, '\n')
-
 
 # examples for "math" module
 print('Round: a / b = ', round(a / b))
